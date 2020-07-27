c2f707f57cac0358260c57a28b1993ab63a39d18
Mario Yepes <marioy47@gmail.com>
2020-07-27 08:44:26 -0500

    Adding templates to the index and detail views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('Hola mundo. Desde la vista index en polls')
+    latests = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latests': latests})
 
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = 'Estas viendo los resultados de la pregunta %s'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('Estas viedo los votos para la pregunta %s' % question_id)
